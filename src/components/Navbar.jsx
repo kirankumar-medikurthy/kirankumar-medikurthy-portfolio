2368879cebb7a255c55fb098c3f688fa08cb0420
kirankumar-medikurthy <kirankumarmedikurthy@gmail.com>
2025-03-25 15:44:20 +0530

    feat: format the navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,7 +64,11 @@ const Navbar = () => {
                   : "text-xl font-medium text-decoration-none whitespace-nowrap text-white"
               }
             >
-              <img src="kirankumar-logo.png" alt="kiran logo" style={{ height: "45px"}}/>
+              <img
+                src="kirankumar-logo.png"
+                alt="kiran logo"
+                style={{ height: "45px" }}
+              />
             </Link>
           </div>
           <div class="hidden justify-between items-center w-full md:flex md:w-auto ">
